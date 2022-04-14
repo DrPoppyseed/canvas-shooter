de93c5807fc2b00a2467d2b8621edbe44e04ff09
DrPoppyseed <peaske16180@gmail.com>
2022-04-15 00:24:06 +0900

    remove dist dir from git

--- src/Player.ts
+++
@@ -1,7 +0,0 @@
-import { Circle } from './Circle'
-
-export default class Player extends Circle {
-  constructor(x: number, y: number, radius: number, color: string) {
-    super(x, y, radius, color)
-  }
-}
